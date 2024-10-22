7141c2e333e4f52324adcc10a78b2c10d2dd8c9d
Dmitrijs Naudzuns <dmitrijs@buffalo.edu>
2024-10-21 21:31:21 -0400

    healthcheck for db service

--- app/main.py
+++ app/main.py
@@ -5,15 +5,10 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.core.db import get_db
 from app.core.user import authenticate_user, create_user
-from app.models.user import User
-from pydantic import BaseModel
+from app.models.user import User, UserCreate
 
 app = FastAPI()
 
-class UserCreate(BaseModel):
-    name: str
-    email: str
-    password: str
 
 @app.get("/")
 def read_root():
@@ -24,15 +19,14 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.post("/register")
 def register(user: UserCreate, db: Session = Depends(get_db)):
     db_user = create_user(db, user.name, user.email, user.password)
     if db_user is None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email already registered"
-        )
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
     return {"message": "User created successfully", "user_id": db_user.id}
+
 
 @app.post("/login")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
