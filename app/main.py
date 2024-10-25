5a685ac8c6abfa98357e89b1e371e6b3caf61de2
Dmitrijs Naudzuns <dmitrijs@buffalo.edu>
2024-10-25 15:32:05 -0400

    basic api router setup

--- app/main.py
+++ app/main.py
@@ -1,19 +1,17 @@
 from typing import Union
 from fastapi.staticfiles import StaticFiles
 import os
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
-from app.core.db import get_db
-from app.core.user import authenticate_user, create_user
-from app.models.user import User, UserCreate
+from fastapi import FastAPI, APIRouter
+from app.routers import auth
 
 app = FastAPI()
+router = APIRouter()
 
 public_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "public")
 frontend_build_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "frontend", "build")
 print(frontend_build_dir)
-
+static_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "static")
+app.include_router(auth.router, prefix="/auth")
 app.mount("/public", StaticFiles(directory=public_dir), name="public")
 app.mount("/", StaticFiles(directory=frontend_build_dir, html=True), name="frontend")
 
@@ -21,28 +19,3 @@ app.mount("/", StaticFiles(directory=frontend_build_dir, html=True), name="front
 @app.get("/api")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/api/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.post("/register")
-def register(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = create_user(db, user.name, user.email, user.password)
-    if db_user is None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
-    return {"message": "User created successfully", "user_id": db_user.id}
-
-
-@app.post("/login")
-def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return {"access_token": user.email, "token_type": "bearer"}
