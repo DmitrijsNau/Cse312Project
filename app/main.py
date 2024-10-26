5e93ab0c3628e9c7bee42a2e29a4311a1af0d689
Bryanv0393 <bvilchis@buffalo.edu>
2024-10-25 23:49:34 -0400

    Like routes added

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from typing import Union
 from fastapi.staticfiles import StaticFiles
 import os
 from fastapi import FastAPI, APIRouter
-from app.routers import auth, pets
+from app.routers import auth, pets, likes
 from app.core.db import init_db
 
 
@@ -19,10 +19,11 @@ app = FastAPI(lifespan=lifespan)
 router = APIRouter()
 public_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "public")
 frontend_build_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "frontend", "build")
-static_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "static")
-app.include_router(auth.router, prefix="/auth")
 app.mount("/public", StaticFiles(directory=public_dir), name="public")
 app.mount("/", StaticFiles(directory=frontend_build_dir, html=True), name="frontend")
+app.include_router(auth.router, prefix="/auth")
+app.include_router(pets.router, prefix="/pets")
+app.include_router(likes.router, prefix="/likes")
 
 
 @app.get("/api")
