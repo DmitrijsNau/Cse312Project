61d2287484c4a2ac90e218ff8fdbefcd479d55d0
Dmitrijs Naudzuns <dmitrijs@buffalo.edu>
2024-10-26 23:41:19 -0400

    figured out routing finally

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-from contextlib import asynccontextmanager
-from typing import Union
-from fastapi.staticfiles import StaticFiles
 import os
-from fastapi import FastAPI, APIRouter
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, APIRouter, Request
+
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from app.routers import auth, pets, likes
 from app.core.db import init_db
+
 
 # create databases on startup
 @asynccontextmanager
@@ -14,17 +16,33 @@ async def lifespan(app: FastAPI):
     pass
 
 
-app = FastAPI(lifespan=lifespan)
-router = APIRouter()
-public_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "public")
-frontend_build_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "frontend", "build")
-app.mount("/public", StaticFiles(directory=public_dir), name="public")
-app.mount("/", StaticFiles(directory=frontend_build_dir, html=True), name="frontend")
-app.include_router(auth.router, prefix="/auth")
-app.include_router(pets.router, prefix="/pets")
-app.include_router(likes.router, prefix="/likes")
+app = FastAPI()
+api = FastAPI(root_path="/api")
+# backend routes
+api_router = APIRouter()
+api.include_router(auth.router, prefix="/auth")
+api.include_router(pets.router, prefix="/pets")
+api.include_router(likes.router, prefix="/likes")
 
 
-@app.get("/api")
+@api.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+# static stuff
+frontend_build_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "frontend", "build")
+
+# serve the stuff
+# Serve the static files from the public and frontend build directories
+app.mount("/api", api)
+
+
+# Catch-all route to serve index.html for any unmatched routes
+@app.get("/{full_path:path}", include_in_schema=False)
+async def catch_all(full_path: str):
+    requested_path = os.path.join(frontend_build_dir, full_path)
+    if os.path.exists(requested_path) and os.path.isfile(requested_path):
+        return FileResponse(requested_path)
+    index_file_path = os.path.join(frontend_build_dir, "index.html")
+    return FileResponse(index_file_path)
--- app/routers/pets.py
+++ app/routers/pets.py
@@ -1,41 +1,33 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.core.auth import get_current_user
-from app.core.user import User 
+from app.core.user import User
 from app.core.db import get_db
-from app.core.pet import (
-    get_pet, create_pet, toggle_like
-)
+from app.models.pet import Pet
+from app.core.pet import get_pet, create_pet, toggle_like
 from app.models.pet import PetCreate, PetResponse
 
-router = APIRouter(
-        tags=["pets"]
-        )
+router = APIRouter(tags=["pets"])
+
 
 @router.post("/create", response_model=PetResponse)
-async def create_new_pet(
-        pet: PetCreate,
-        db: Session = Depends(get_db),
-        author: User = Depends(get_current_user)
-        ):
+async def create_new_pet(pet: PetCreate, db: Session = Depends(get_db), author: User = Depends(get_current_user)):
     try:
         db_pet = create_pet(db, pet.model_dump(), author.id)
         return db_pet
     except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=str(e)
-        )
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
 
 @router.get("/{pet_id}", response_model=PetResponse)
-async def read_pet(
-    pet_id: int,
-    db: Session = Depends(get_db)
-):
+async def read_pet(pet_id: int, db: Session = Depends(get_db)):
     pet = get_pet(db, pet_id)
     if pet is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Pet not found"
-        )
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Pet not found")
     return pet
+
+
+@router.get("/get_all", response_model=list[PetResponse])
+async def get_all_pets(db: Session = Depends(get_db)):
+    pets = db.query(Pet).all()
+    return pets
