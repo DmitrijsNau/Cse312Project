99232ab92399afdd2a2f4269e93bfd63c71bebe6
marduk <mike@luxvera.com>
2024-10-27 21:15:25 -0400

    Remove whack config

--- app/core/config.py
+++ app/core/config.py
@@ -1,19 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-# Load .env file
 load_dotenv()
 
 
 class _Config:
-    # Determine if running in Docker
     IS_DOCKER = os.getenv("DOCKER_ENV") == "docker"
 
-    # Set database URLs based on environment
     if IS_DOCKER:
-        DATABASE_URL = os.getenv("DATABASE_URL", "").replace("postgres://", "postgresql://")
+        DATABASE_URL = os.getenv("DATABASE_URL", "")
     else:
-        DATABASE_URL = os.getenv("LOCAL_DATABASE_URL", "").replace("postgres://", "postgresql://")
+        DATABASE_URL = os.getenv("LOCAL_DATABASE_URL", "")
 
 
 config = _Config()
